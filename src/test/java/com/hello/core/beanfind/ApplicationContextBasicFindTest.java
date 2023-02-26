dccc1965578cf20ee24fd9e9da47b9c5fdacddee
hagnoykmik <ymj02148@naver.com>
2023-02-27 03:01:58 +0900

    0227

--- src/test/java/com/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/com/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,7 +6,10 @@ import com.hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * packageName    : com.hello.core.beanfind fileName       : ApplicationContextBasicFindTest author
@@ -42,4 +45,19 @@ public class ApplicationContextBasicFindTest {
     MemberService memberService = ac.getBean("memberService", MemberService.class);
     Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
   }
+
+  //실패 test
+  @Test
+  @DisplayName("빈 이름으로 조회X")
+  public void findBeanByNameX() {
+    // ac.getBean("xxxxx), MemberService.class);
+//    MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+    assertThrows(NoSuchBeanDefinitionException.class,                      // NoSuchBeanDefinitionException.class 예외가 터져야 함
+            () -> ac.getBean("xxxxx", MemberService.class));         // 아래 로직을 실행하면
+  }
+
+
+
 }
+
+
